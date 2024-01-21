7281d8b4b4d28d256eb81f5406db61a6a388b6f0
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-20 21:57:52 -0800

    pass bread constructor test

---
+++ Bakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Bread testBread = new Bread();
+      Console.WriteLine(testBread.GetType());
+    }
+  }
+}
