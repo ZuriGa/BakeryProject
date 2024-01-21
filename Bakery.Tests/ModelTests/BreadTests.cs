f64783a4ff114b3b18310d0d99a3d5618165fe5d
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-20 23:03:39 -0800

    add passing test, code for bread.quantity SET

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -9,18 +9,27 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread(4);
+      Bread newBread = new Bread(1);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
     }
     
     [TestMethod]
     public void GetQuantity_ReturnsQuantity_Int()
     {
-      int orderQuantity = 4;
+      int orderQuantity = 1;
       Bread newBread = new Bread(orderQuantity);
       int result = newBread.Quantity;
       Assert.AreEqual(orderQuantity, result);
       
+    }
+
+    [TestMethod]
+    public void SetQuantity_SetsValueOfQuantity_Void()
+    {
+      Bread newBread = new Bread(2);
+      int newOrderQuantity = 4;
+      newBread.Quantity = newOrderQuantity;
+      Assert.AreEqual(newOrderQuantity, newBread.Quantity);
     }
   }
 }
--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -2,7 +2,7 @@ namespace Bakery.Models
 {
   public class Bread
   {
-    public int Quantity { get; } 
+    public int Quantity { get; set; } 
     public Bread(int quantity)
     {
       Quantity = quantity;
