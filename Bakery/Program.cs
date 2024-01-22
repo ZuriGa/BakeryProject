b0ce8a9342f4b34c0da96ca8f9b8770bb6b2f614
ZuriGa <zurisadailopezmartinez@gmail.com>
2024-01-21 19:50:53 -0800

    UI logic is updated and working.

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,6 +1,6 @@
 using System;
 using Bakery.Models;
-using System.Collections.Generic;
+
 
 
 namespace Bakery
@@ -9,25 +9,28 @@ namespace Bakery
   {
     static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("           ~~ Welcome to Pierre's Bakery! ~~");
       Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
-      OrderDisplay();
-      Console.WriteLine("                 ~ Goodbye ~");
+      BakeryMenu();
+      
+      Console.WriteLine("                 ~~ Goodbye ~~");
       Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
     }
 
-    static void OrderDisplay()
+    static void BakeryMenu()
     {
       int breadQuantity;
       int pastriesQuantity;
-      Bread newBread;
-      Pastry newPastry;
+      Bread newBread = null;
+      Pastry newPastry = null;
+
 
       Console.WriteLine("Pierre offers the following items:");
       Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....--....-  ");
       Console.WriteLine("|    Bread:  | Buy 2, get 1 free - Single loaf $5.00    |");
       Console.WriteLine("|    Pastry: | Buy 3, get 1 free - Single Pastry $2.00  |");
       Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....--....-  ");
+
       Console.Write("   Enter the quantity of Bread loaves: ");
       breadQuantity = int.Parse(Console.ReadLine());
       Console.Write("   Enter the quantity of Pastries: ");
@@ -36,48 +39,60 @@ namespace Bakery
       newBread = new Bread(breadQuantity);
       newPastry = new Pastry(pastriesQuantity);
 
+      OrderDisplay(newBread, newPastry);
+    }
 
+    static void OrderDisplay(Bread newBread, Pastry newPastry)
+    {
       try
       {
-        int breadOrder = int.Parse(Console.ReadLine());
-        newBread.Quantity = breadOrder;
         newBread.CalculateTotalCost();
         int breadCost = newBread.TotalCost;
 
-        int pastryOrder = int.Parse(Console.ReadLine());
-        newPastry.Quantity = pastryOrder;
         newPastry.CalculateTotalCost();
         int pastryCost = newPastry.TotalCost;
-
+        Console.WriteLine("n/");
         Console.WriteLine("              Here's your order:");
         Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
-        Console.WriteLine("           Bread Order x {1}: {0:C}", newBread.TotalCost, newBread.Quantity);
-        Console.WriteLine("          Pastry Order x {1}: {0:C}", newPastry.TotalCost, newPastry.Quantity);
-        Console.WriteLine("          Total Order Cost: {0:C}", breadCost + pastryCost);
+        Console.WriteLine($"           Bread Order x {newBread.Quantity}: {newBread.TotalCost:C}");
+        Console.WriteLine($"          Pastry Order x {newPastry.Quantity}: {newPastry.TotalCost:C}");
+        Console.WriteLine($"          Total Order Cost: {breadCost + pastryCost:C}");
         Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
-
+        ConfirmOrder(newBread, newPastry);
       }
       catch
       {
         Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
         Console.WriteLine("     !     Unable to complete order!     !");
-        Console.WriteLine("     !  Please enter only whole numbers  !");
+        Console.WriteLine("     !  Please enter only numbers  !");
         Console.WriteLine("-....- -....- -....- -....- -....- -....- -....--....- ");
-        OrderDisplay();
+        OrderDisplay(newBread, newPastry);
       }
     }
 
-    static void ConfirmOrder()
+    static void ConfirmOrder(Bread newBread, Pastry newPastry)
     {
-      Console.WriteLine("    Would you like to purchase more items?  ");
-      Console.WriteLine("       To add more, type 'yes'.");
-      Console.WriteLine("        To exit, enter any other key.");
+      Console.WriteLine("    Would you like to complete your purchase or edit your order?  ");
+      Console.WriteLine("       To edit your order type 'e'.");
+      Console.WriteLine("        To Complete order type 'c', to exit enter any other key.");
       string userInput = Console.ReadLine();
 
       if (userInput == "yes" || userInput == "Yes")
       {
-        OrderDisplay();
+        BakeryMenu();
+      }
+      else if (userInput == "no" || userInput == "No")
+      {
+        Console.WriteLine($"Your final order: Bread x {newBread.Quantity}, Pastry x {newPastry.Quantity}");
+        Console.WriteLine($"Total Order Cost: {newBread.TotalCost + newPastry.TotalCost:C}");
+      }
+      else
+      {
+        
       }
     }
+
+
+
   }
 }
